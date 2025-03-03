ae9a12a3981c33628f288e69c057d630de37c745
Mark McKenna <mark.mckenna@redspace.com>
2025-03-02 22:44:04 -0400

    WIP

---
+++ kmp/src/commonMain/kotlin/api.kt
@@ -0,0 +1,17 @@
+package lci
+
+// TODO: Generate a class that contains typed methods based on the exported members of the Javascript source
+
+interface Function {
+}
+
+interface FunctionBearing {
+  fun function(name: String): Function
+}
+
+interface ClientAdapter : FunctionBearing
+
+interface Container : FunctionBearing
+
+
+1-800-698-9034
